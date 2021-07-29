67ca99f2de4f299e7bdfb50a42dc72688adef779
hann <hmweeks93@gmail.com>
2021-07-29 11:19:37 -0500

    updates to request handler

--- request_handler.py
+++ request_handler.py
@@ -80,17 +80,14 @@ class HandleRequests(BaseHTTPRequestHandler):
                     response = f"{get_single_post(id)}"
                 else:
                     response = f"{get_all_posts()}"
-<<<<<<< HEAD
             elif resource == "category":
                 if id is not None: 
                     response = f"{get_posts_by_category_id(id)}"
                 else: 
                     response = f"{get_all_posts()}"
-=======
             elif resource == "myposts":
                 if id is not None:
                     response = f"{get_posts_by_user_id(id)}"
->>>>>>> main
 
         # elif len(parsed) == 3:
         #     (resource, key, value) = parsed
