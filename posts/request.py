3f7e0be2f8dc9fd6dceb0daf4ddc3cd5a6af5554
Jonathan Watson <thejmdw@gmail.com>
2021-07-29 16:30:26 -0400

    can edit post

--- posts/request.py
+++ posts/request.py
@@ -167,3 +167,36 @@ def create_post(new_post):
 
 
     return json.dumps(new_post)
+
+# SQL UPDATE POST
+def update_post(id, new_post):
+    # iterate throught the postS list, but use enumerate() so that
+    # you can access the index of each item
+    with sqlite3.connect("./rare.db") as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        UPDATE Posts
+            SET
+                user_id = ?,
+                category_id = ?,
+                title = ?,
+                publication_date = ?,
+                image_url = ?,
+                content = ?,
+                approved = ?
+        WHERE id = ?
+        """, (new_post['user_id'], new_post['category_id'],
+              new_post['title'], new_post['publication_date'],
+              new_post['image_url'], new_post['content'], new_post['approved'], id, ))
+
+        # Were any rows affected?
+        # Did the client send an `id` that exists?
+        rows_affected = db_cursor.rowcount
+
+    if rows_affected == 0:
+        # Forces 404 response by main module
+        return False
+    else:
+        # Forces 204 response by main module
+        return True
